6407856c87430fd08b0ce20d1c306ba1ec28de31
trungenglish <nguyentrung23232323@gmail.com>
2025-09-09 17:05:33 +0700

    feat(footer): finish footer
    build foote have social, logo, contact, policy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import HeroSection from "@/features/home/components/HeroSection";
+import {featuredMovie} from "@/constants/mockData";
 
 export default function Home() {
   return (
     <>
-      <HeroSection  />
+      <HeroSection movie={featuredMovie}/>
       <div className="space-y-8 pb-16">
         {/*<MovieSection title="Phim Thịnh Hành" movies={trendingMovies} />*/}
         {/*<MovieSection title="Phim Bộ Mới" movies={tvSeries} />*/}
