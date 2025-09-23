c3cf259ac9096ac3d2275239e20d3671b3616420
trungenglish <nguyentrung23232323@gmail.com>
2025-09-23 23:19:05 +0700

    fix(countrymovie): fix button navigation

--- src/features/home/components/CountryMoviesCarousel.tsx
+++ src/features/home/components/CountryMoviesCarousel.tsx
@@ -49,18 +49,14 @@ const CountryMoviesCarousel = ({movies}: CountryMoviesCarouselProps) => {
                 </div>
 
                 {/* Carousel Section */}
-                <div className="flex-1 min-w-0">
+                <div className="flex-1 min-w-0 relative">
                   {/* Navigation Buttons */}
-                  <div className="absolute top-[40%] -translate-y-1/2 z-20 flex justify-between w-full">
-                    <button className={`nav-prev-${title.id} left-0 w-10 h-10 bg-white hover:bg-gray-50 rounded-full shadow-lg border border-gray-200
-                      transition-all duration-300 hover:scale-110 hover:shadow-xl active:scale-95 text-gray-600 hover:text-gray-800 -ml-6 cursor-pointer ${isBeginning ? 'hidden' : ''}`}>
-                      <FontAwesomeIcon icon={faChevronLeft} size="sm"/>
-                    </button>
-                    
-                    <button className={`nav-next-${title.id} right-0 w-10 h-10 bg-white hover:bg-gray-50 rounded-full shadow-lg border border-gray-200 transition-all duration-300 hover:scale-110 hover:shadow-xl active:scale-95 text-gray-600 hover:text-gray-800 mr-48 cursor-pointer ${isEnd ? 'hidden' : ''} `}>
-                      <FontAwesomeIcon icon={faChevronRight} size="sm"/>
-                    </button>
-                  </div>
+                  <button className={`nav-prev-${title.id} absolute left-0 top-[40%] -translate-x-1/2 -translate-y-1/2 z-20 w-10 h-10 bg-white hover:bg-gray-50 rounded-full shadow-lg border border-gray-200 transition-all duration-300 hover:scale-110 hover:shadow-xl active:scale-95 text-gray-600 hover:text-gray-800 cursor-pointer ${isBeginning ? 'hidden' : ''}`}>
+                    <FontAwesomeIcon icon={faChevronLeft} size="sm"/>
+                  </button>
+                  <button className={`nav-next-${title.id} absolute right-0 top-[40%] translate-x-1/2 -translate-y-1/2 z-20 w-10 h-10 bg-white hover:bg-gray-50 rounded-full shadow-lg border border-gray-200 transition-all duration-300 hover:scale-110 hover:shadow-xl active:scale-95 text-gray-600 hover:text-gray-800 cursor-pointer ${isEnd ? 'hidden' : ''}`}>
+                    <FontAwesomeIcon icon={faChevronRight} size="sm"/>
+                  </button>
                   
                   <Swiper
                     modules={[Navigation]}
